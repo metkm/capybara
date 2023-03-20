62debd12697c45c62152e210824bfe16376d3a9d
metkm <metinkorkmaz417@gmail.com>
2023-03-20 20:19:28 +0300

    update css for better mobile support

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,15 +1,15 @@
 <template>
   <section class="bg-[#002BFB]">
-    <div class="base-text grid h-full place-content-center">
+    <div class="base-text grid h-full place-content-center overflow-x-hidden">
       <p class="text-5xl ml-10">カピバラ</p>
-      <p class="base-text">CAPY BARA</p>
+      <p class="base-text whitespace-nowrap">CAPY BARA</p>
     </div>
     <p class="big-text behind-text">カピバラ</p>
   </section>
-  <section class="bg-[#7FEFFD] text-[#F1FF8F] p-10">
-    <p class="base-text max-w-screen-xl -mt-5">LARGEST LORENT <br> IN THE WORLD</p>
+  <section class="bg-[#7FEFFD] text-[#F1FF8F] p-5">
+    <p class="base-text">LARGEST <br> LORENT <br> IN THE <br> WORLD</p>
   </section>
   <section class="bg-[#E7E3FD] text-[#FF2663]">
-    <p class="base-text flex h-full justify-end items-center pr-60">NO <br> TAIL</p>
+    <p class="base-text flex h-full justify-end items-center xl:pr-64">NO <br> TAIL</p>
   </section>
 </template>
